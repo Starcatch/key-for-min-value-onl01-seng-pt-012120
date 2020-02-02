04db57fc205b23954fac1e207b72418571ccee23
Timur Mishiev <timurmishiev@gmail.com>
2020-02-02 00:08:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,14 @@
  name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  min_value = nil 
-  min_key = nil
-  name_hash.each do |key, value|
-    
-    
+  smallest_key = nil
+  smallest_value = nil 
 
-end
+  name_hash.each do |key, value|
+     if smallest_value == nil || value < smallest_value
+       smallest_value = value
+       smallest_key = key
+      end 
+    end 
+    smallest_key
+  end
